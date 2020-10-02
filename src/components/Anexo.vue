91ba68fe07edb35d20d15a326864bb1bed1d775f
katob <labotsirc.1997@gmail.com>
2020-10-02 20:07:20 -0300

    Prueba

---
+++ src/components/Anexo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <form>
+      <label for="fname">First name:</label><br>
+      <input type="text" id="fname" name="fname"><br>
+      <label for="lname">Last name:</label><br>
+      <input type="text" id="lname" name="lname">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Anexo"
+}
+</script>
+
+<style scoped>
+
+</style>
